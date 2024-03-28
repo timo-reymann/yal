f8dc54909759c841fc34b823dc07ed682c22d18e
Timo Reymann <timo.reymann@deepl.com>
2024-03-28 21:49:16 +0100

    chore: Add tests that default template renders

---
+++ pkg/templating/main_test.go
@@ -0,0 +1,38 @@
+package templating
+
+import (
+	"github.com/timo-reymann/yal/pkg/config"
+	"github.com/timo-reymann/yal/pkg/model"
+	"testing"
+)
+
+var testConfig = &config.Configuration{
+	Sections:      []model.Section{},
+	SearchEngines: []model.SearchEngine{},
+	Assets: config.Assets{
+		Logo:       "",
+		Mascot:     "",
+		Background: "",
+		Favicon:    "",
+	},
+}
+
+func TestRenderTemplate_Default(t *testing.T) {
+	templated, err := RenderTemplate("builtin", testConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if templated == "" {
+		t.Fatal("Expected template to be rendered for default")
+	}
+}
+
+func TestRenderTemplate_DefaultFile(t *testing.T) {
+	templated, err := RenderTemplate("index.gohtml", testConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if templated == "" {
+		t.Fatal("Expected template to be rendered for default")
+	}
+}
